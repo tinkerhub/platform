5dbb5a584239461a5a2d294bdcba5100df942f7e
Sera Mariam George <leelasera19@gmail.com>
2022-05-31 14:05:10 +0530

    feat: Integrated prisma as plugin along with fastify

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import fastify, { FastifyInstance } from 'fastify';
 import { IncomingMessage, Server, ServerResponse } from 'http';
 import { nanoid } from 'nanoid';
 import pino, { Logger } from 'pino';
+import { PrismaClient } from '@prisma/client'
 
 import { fastifyLogger } from './logger';
 import { envConfig } from './env';
 
 import { miscRoutes } from './misc';
 import { authRoutes } from './auth';
+import prismaPlugin from '../plugins/prisma'
 
 const server: FastifyInstance = fastify<Server, IncomingMessage, ServerResponse, Logger>({
   logger: pino({
@@ -26,10 +28,10 @@ server.register(fastifyLogger);
 server.register(authRoutes, { prefix: '/auth' });
 server.register(miscRoutes);
 
+server.register(prismaPlugin);
 const start = async () => {
   try {
     await server.after();
-
     await server.listen(server.config.PORT, server.config.HOST);
   } catch (err) {
     server.log.error(err);
@@ -37,3 +39,31 @@ const start = async () => {
   }
 };
 start();
+const prisma = new PrismaClient({})
+
+// A `main` function so that you can use async/await
+async function main() {
+  // Create user, posts, and categories
+  // const user = await prisma.user.create({
+  //   data: {
+  //     email: 'ariadne@prisma.io',
+  //     name: 'Ariadne',
+  //     posts: {
+  //       create: [
+  //         {
+  //           title: 'My first day at Prisma',
+           
+  //         },
+          
+          
+  //       ],
+  //     },
+  //   },
+  // })
+// console.log(user);
+
+const users= await prisma.user.findMany()
+console.log(users);
+
+}
+main()
