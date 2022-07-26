abbcc28308bbfec572ff10cb3ead4211c607628c
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-07-26 18:16:39 +0530

    fix: fixed linting issue

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,27 +1,53 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:@next/next/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'import'],
   rules: {
+    'react/prop-types': 0,
+    indent: ['error', 2],
+    'linebreak-style': 0,
     'prettier/prettier': [
       'error',
       {
         endOfLine: 'auto',
       },
     ],
-    'import/prefer-default-export': 'off',
-    'class-methods-use-this': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', 'tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-var-requires': 'off',
   },
-  root: true,
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
 export default MyApp;
