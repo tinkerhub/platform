66dda9fc413e637497fbbf9d25dec05b9eba1cc8
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-09-16 15:14:43 +0530

    fix: conflicts

--- apps/web/hooks/useData.ts
+++ apps/web/hooks/useData.ts
@@ -7,21 +7,26 @@ export const useData = (dataField: Form) => {
   const [isError, setError] = useState<any>(null);
   const [dataInfo, setData] = useState<any>(null);
   const sendData = async () => {
-    setIsLoading(true);
     try {
+      setIsLoading(true);
       const { data } = await apiHandler.post('/user', dataField);
       if (!data.ok) throw new Error(data.message);
       setData(data);
     } catch (e) {
       setError(e);
+    } finally {
+      setIsLoading(false);
     }
-    setIsLoading(false);
   };
-  sendData();
 
-  return {
+  const Properties = {
     data: dataInfo,
     isLoading,
     error: isError,
+  };
+
+  return {
+    sendData,
+    Properties,
   };
 };
