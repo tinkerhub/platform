86c072041e69058ffd33b6aeabf699270cde098e
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-12-26 22:51:55 +0530

    fix: removed ST prebuilt ui

--- apps/web/auth/index.tsx
+++ apps/web/auth/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import Passwordless from 'supertokens-auth-react/recipe/passwordless';
 import Session from 'supertokens-auth-react/recipe/session';
-import { userInputCodeFormStyle, emailOrPhoneFormStyle } from './OverrideStyle';
 import { ENV } from '../config';
 
 export const authConfig = () => ({
@@ -18,24 +17,10 @@ export const authConfig = () => ({
       contactMethod: 'PHONE',
       palette: {
         textLabel: '#adb5bd',
-        //   textTitle: localStorage.getItem('chakra-ui-color-mode') === 'light' ? 'black' : 'white',
       },
       signInUpFeature: {
         defaultCountry: 'IN',
-        userInputCodeFormStyle,
-        emailOrPhoneFormStyle,
       },
-      // getRedirectionURL: async (context) => {
-      //   // called on a successful sign in / up. Where should the user go next?
-      //   if (context.action === 'SUCCESS') {
-      //     const isWizardCompleted = localStorage.getItem('isWizardCompleted'); // returns null or "YES"
-      //     if (isWizardCompleted) {
-      //       // user signed up and have wizardform completed
-      //       return '/profile'; // redirect to profile page
-      //     }
-      //   }
-      //   return undefined;
-      // },
     }),
     Session.init(),
   ],
