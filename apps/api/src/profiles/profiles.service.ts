0c39f261c28de7a54d93461eecc43191b533c26d
reenphygeorge <reenphygeorge@gmail.com>
2023-01-12 18:32:07 +0530

    fix: casing in success response

--- apps/api/src/profiles/profiles.service.ts
+++ apps/api/src/profiles/profiles.service.ts
@@ -17,7 +17,7 @@ export class ProfilesService {
   // Response Handler
   Success(resp: Resp) {
     return {
-      Success: true,
+      success: true,
       message: resp.message,
       data: resp.data,
     };
