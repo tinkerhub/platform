db2db3fc094c011b2e8cc690df474740969553ba
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-08-09 22:29:11 +0530

    fix: discord url as env

--- apps/web/views/wizard/components/Final.tsx
+++ apps/web/views/wizard/components/Final.tsx
@@ -22,6 +22,7 @@ import { LottieAnim } from './Lottie';
 import { Oops } from './Oops';
 import { useAuthCtx } from '../../../hooks';
 import { Form } from '../../../types';
+import { ENV } from '../../../config';
 
 interface Prop {
   isLoading: boolean;
@@ -199,7 +200,7 @@ export const Final = ({ isLoading, id = 'ERROR', error, user }: Prop) => {
             2
           </Center>
           <Heading size="md">Go to TinkerHub discord</Heading>
-          <a href="https://discord.gg/j7vDXrD6Ss">
+          <a href={ENV.discordUrl}>
             <Flex
               mt="15px"
               onClick={copyFile}
