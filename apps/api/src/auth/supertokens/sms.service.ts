a2c0646f42c0f24347514fe5b31f892682faa9a3
Rohit T P <tprohit9@gmail.com>
2023-10-04 20:36:51 +0530

    changed to otp

--- apps/api/src/auth/supertokens/sms.service.ts
+++ apps/api/src/auth/supertokens/sms.service.ts
@@ -1,39 +1,39 @@
 // @ts-ignore
-import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { catchError, firstValueFrom } from 'rxjs';
+import {HttpService} from '@nestjs/axios';
+import {Injectable} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {catchError, firstValueFrom} from 'rxjs';
 
 @Injectable()
 export class SmsService {
-  constructor(private readonly httpService: HttpService, private configService: ConfigService) {}
+    constructor(private readonly httpService: HttpService, private configService: ConfigService) {
+    }
 
-  async smsSend(phone: string, otp: string) {
-    const headerValues = {
-      authkey: this.configService.get<string>('MSG91_AUTH_KEY') as string,
-      'content-type': 'application/json' as string,
-    };
+    async smsSend(phone: string, otp: string) {
+        const headerValues = {
+            authkey: this.configService.get<string>('MSG91_AUTH_KEY') as string,
+            'content-type': 'application/json' as string,
+        };
 
-    await firstValueFrom(
-      this.httpService
-        .post(
-          this.configService.get<string>('MSG91_API_DOMAIN') as string,
-          {
-            flow_id: this.configService.get<string>('MSG91_FLOW_ID'),
-            sender: this.configService.get<string>('MSG91_SENDER_ID'),
-            short_url: this.configService.get<string>('MSG91_SHORT_URL'),
-            mobiles: phone,
-            OTP: otp,
-          },
-          {
-            headers: headerValues,
-          }
-        )
-        .pipe(
-          catchError((error: any) => {
-            throw error;
-          })
-        )
-    );
-  }
+        await firstValueFrom(
+            this.httpService
+                .post(
+                    this.configService.get<string>('MSG91_API_DOMAIN') as string,
+                    {
+                        template_id: this.configService.get<string>('MSG91_FLOW_ID'),
+                        invisible: 0,
+                        mobile: phone,
+                        otp,
+                    },
+                    {
+                        headers: headerValues,
+                    }
+                )
+                .pipe(
+                    catchError((error: any) => {
+                        throw error;
+                    })
+                )
+        );
+    }
 }
