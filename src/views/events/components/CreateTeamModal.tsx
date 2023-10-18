0c2fa4d39f21c9b96aa95500258d8ede65efbfcd
Yan Chummar <yanpooppada@gmail.com>
2023-10-19 00:12:46 +0530

    added success content in create team modal

--- src/views/events/components/CreateTeamModal.tsx
+++ src/views/events/components/CreateTeamModal.tsx
@@ -10,8 +10,9 @@ import {
   Input,
   ModalFooter,
   Button,
+  Text
 } from '@chakra-ui/react';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 interface Disclosure {
   isOpen: boolean;
@@ -21,39 +22,61 @@ interface Disclosure {
 
 export const CreateTeamModal = ({ isOpen, onClose, handleModalAction }: Disclosure) => {
 
+  const [isCreated, setIsCreated] = useState<boolean>(false);
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose} >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Create your team!</ModalHeader>
+        <ModalHeader>{ isCreated ? 'Congratulations! 🎉': 'Create your team!'}</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
-          <FormControl>
-            <FormLabel>Github ID of Team Member #1 (Yourself)</FormLabel>
-            <Input placeholder='eg. @yanchummar' />
-          </FormControl>
-          <FormControl mt={4}>
-            <FormLabel>Github ID of Team Member #2</FormLabel>
-            <Input placeholder='eg. @rohittp0' />
-          </FormControl>
-          <FormControl mt={4}>
-            <FormLabel>Github ID of Team Member #3</FormLabel>
-            <Input placeholder='eg. @appukurian' />
-          </FormControl>
-          <FormControl mt={4}>
-            <FormLabel>Github ID of Team Member #4 (Optional)</FormLabel>
-            <Input placeholder='eg. @akhilmhdh' />
-          </FormControl>
+          {
+            isCreated ? (
+              <>
+                <Text
+                  as="b">
+                  Your team is registered for StackUp!
+                </Text>
+                <Text mt={2}>
+                  Make sure that all of the team members check their emails to join this team and complete their registration.
+                </Text>
+              </>
+            ) : (
+              <>
+                <FormControl>
+                  <FormLabel>Github ID of Team Member #1 (Yourself)</FormLabel>
+                  <Input placeholder='eg. @yanchummar' />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Github ID of Team Member #2</FormLabel>
+                  <Input placeholder='eg. @rohittp0' />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Github ID of Team Member #3</FormLabel>
+                  <Input placeholder='eg. @appukurian' />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Github ID of Team Member #4 (Optional)</FormLabel>
+                  <Input placeholder='eg. @akhilmhdh' />
+                </FormControl>
+              </>
+            )
+          }
         </ModalBody>
-
-        <ModalFooter>
-          <Button onClick={onClose} mr={3}>Cancel</Button>
-          <Button colorScheme='blue'>
-            Create Team
-          </Button>
-        </ModalFooter>
+        
+        {
+          !isCreated ? (
+            <ModalFooter>
+              <Button onClick={onClose} mr={3}>Cancel</Button>
+              <Button colorScheme='blue'>
+                Create Team
+              </Button>
+            </ModalFooter>
+          ) : false
+        }
       </ModalContent>
     </Modal>
   );
